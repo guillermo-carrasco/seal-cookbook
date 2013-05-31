50d9de90da076cbd3307e49e3a7e6e2c8aee5b1b
Guillermo Carrasco <guillermo.carrasco@scilifelab.se>
2013-05-31 12:58:06 +0000

    Pint to my codebase, compatible with CDH4.2.x

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
-default['seal-latest-code']   =   'http://git.code.sf.net/p/biodoop-seal/code'
+#default['seal-latest-code']   =   'http://git.code.sf.net/p/biodoop-seal/code'
+default['seal-latest-code']   =   'http://github.com/guillermo-carrasco/biodoop-seal-code.git'
 default['tmp']                =   '/tmp/seal'
 default['install_dir']        =   '/opt'
